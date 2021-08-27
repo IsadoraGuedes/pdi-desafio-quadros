9e80a368617f43d7f7ef7a667643c613a899ce4a
Isadora Guedes <isasoaresguedes@gmail.com>
2021-08-27 20:12:08 -0300

    altera path public

--- services/api.ts
+++ services/api.ts
@@ -4,5 +4,5 @@ export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
     process.env.VERCEL_URL ||
-    'https://pdi-desafio-quadros.herokuapp.com/',
+    'https://pdi-desafio-quadros.herokuapp.com/public/',
 })
